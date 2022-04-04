32c55d307f4af366b319fced0e7324b8eb3310e6
ppunikim <yd62322@naver.com>
2022-04-04 18:03:32 +0900

    4월4일 오후수업

---
+++ After_study/src/com/callor/first/First.java
@@ -0,0 +1,17 @@
+package com.callor.first;
+
+public class First {
+	public static void main(String[] args) {
+		print(a,b);
+	}
+	
+	public static print(a,b) {
+		
+		int a;
+		int b;
+		System.out.println(a + b);
+	}
+
+}
+
+
